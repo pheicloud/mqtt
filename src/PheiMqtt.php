55577e87de3143a6da5eca800320f9508ccb1854
shine1rainbow <shine1rainbow@gmail.com>
2019-04-29 09:28:35 +0800

    sync

--- src/PheiMqtt.php
+++ src/PheiMqtt.php
@@ -41,6 +41,7 @@ class PheiMqtt
 		$this->cafile = $cafile;
 	}
 
+    /* auto connect config */
     public function connect_auto($clean = true, $will = NULL, $username = NULL, $password = NULL)
     {
         while ($this->connect($clean, $will, $username, $password) == false)
@@ -103,21 +104,21 @@ class PheiMqtt
 		$buffer .= chr($this->keepalive & 0xff); $i++;
 		$buffer .= $this->strwritestring($this->clientid,$i);
 		//Adding will to payload
-		if($this->will != NULL){
+		if ($this->will != NULL) {
 			$buffer .= $this->strwritestring($this->will['topic'],$i);  
 			$buffer .= $this->strwritestring($this->will['content'],$i);
 		}
-		if($this->username) $buffer .= $this->strwritestring($this->username,$i);
-		if($this->password) $buffer .= $this->strwritestring($this->password,$i);
+		if ($this->username) $buffer .= $this->strwritestring($this->username,$i);
+		if ($this->password) $buffer .= $this->strwritestring($this->password,$i);
 		$head = "  ";
 		$head{0} = chr(0x10);
 		$head{1} = chr($i);
 		fwrite($this->socket, $head, 2);
 		fwrite($this->socket,  $buffer);
 	 	$string = $this->read(4);
-		if(ord($string{0})>>4 == 2 && $string{3} == chr(0)){
-			if($this->debug) echo "Connected to Broker\n"; 
-		}else{	
+		if (ord($string{0})>>4 == 2 && $string{3} == chr(0)) {
+			if ($this->debug) echo "Connected to Broker\n";
+		} else {
 			error_log(sprintf("Connection failed! (Error: 0x%02x 0x%02x)\n", 
 			                        ord($string{0}),ord($string{3})));
 			return false;
@@ -127,8 +128,8 @@ class PheiMqtt
 	}
 
 	/* read: reads in so many bytes */
-	function read($int = 8192, $nb = false){
-		
+    public function read($int = 8192, $nb = false)
+    {
 		$string="";
 		$togo = $int;
 		
@@ -153,8 +154,7 @@ class PheiMqtt
 		$id = $this->msgid;
 		$buffer .= chr($id >> 8);  $i++;
 		$buffer .= chr($id % 256);  $i++;
-        foreach($topics as $key => $topic)
-        {
+        foreach ($topics as $key => $topic) {
 			$buffer .= $this->strwritestring($key, $i);
 			$buffer .= chr($topic["qos"]); $i++;
 			$this->topics[$key] = $topic; 
@@ -180,7 +180,7 @@ class PheiMqtt
         $head = chr(0xc0);		
         $head .= chr(0x00);
         fwrite($this->socket, $head, 2);
-        if($this->debug) echo "ping sent\n";
+        if ($this->debug) echo "ping sent\n";
 	}
 
 	/* disconnect: sends a proper disconect cmd */
@@ -206,7 +206,7 @@ class PheiMqtt
 		$buffer = "";
 		$buffer .= $this->strwritestring($topic,$i);
 		//$buffer .= $this->strwritestring($content,$i);
-		if($qos){
+		if ($qos) {
 			$id = $this->msgid++;
 			$buffer .= chr($id >> 8);  $i++;
 		 	$buffer .= chr($id % 256);  $i++;
@@ -215,8 +215,8 @@ class PheiMqtt
 		$i+=strlen($content);
 		$head = " ";
 		$cmd = 0x30;
-		if($qos) $cmd += $qos << 1;
-		if($retain) $cmd += 1;
+		if ($qos) $cmd += $qos << 1;
+		if ($retain) $cmd += 1;
 		$head{0} = chr($cmd);		
 		$head .= $this->setmsglength($i);
 		fwrite($this->socket, $head, strlen($head));
@@ -230,65 +230,66 @@ class PheiMqtt
         $topic = substr($msg,2,$tlen);
         $msg = substr($msg,($tlen+2));
         $found = 0;
-        foreach($this->topics as $key=>$top){
-            if( preg_match("/^".str_replace("#",".*",
+        foreach ($this->topics as $key=>$top) {
+            if ( preg_match("/^".str_replace("#",".*",
                     str_replace("+","[^\/]*",
                         str_replace("/","\/",
                             str_replace("$",'\$',
                                 $key))))."$/",$topic) ){
-                if(is_callable($top['function'])){
+                if (is_callable($top['function'])) {
                     call_user_func($top['function'],$topic,$msg);
                     $found = 1;
                 }
             }
         }
 
-        if($this->debug && !$found) echo "msg recieved but no match in subscriptions\n";
+        if ($this->debug && !$found) echo "msg recieved but no match in subscriptions\n";
 	}
 
 	/* proc: the processing loop for an "allways on" client 
 		set true when you are doing other stuff in the loop good for watching something else at the same time */	
     public function proc( $loop = true)
     {
-		if(1){
+		if (1) {
 			$sockets = array($this->socket);
 			$w = $e = NULL;
 			$cmd = 0;
 			
 				//$byte = fgetc($this->socket);
-			if(feof($this->socket)){
-				if($this->debug) echo "eof receive going to reconnect for good measure\n";
+			if (feof($this->socket)) {
+				if ($this->debug) echo "eof receive going to reconnect for good measure\n";
 				fclose($this->socket);
 				$this->connect_auto(false);
-				if(count($this->topics))
+				if (count($this->topics))
 					$this->subscribe($this->topics);	
 			}
 			
 			$byte = $this->read(1, true);
 			
-			if(!strlen($byte)){
-				if($loop){
+			if (!strlen($byte)) {
+				if ($loop) {
 					usleep(100000);
 				}
 			 
-			}else{ 
+			} else {
 			
 				$cmd = (int)(ord($byte)/16);
-				if($this->debug) echo "Recevid: $cmd\n";
+				if ($this->debug) echo "Recevid: $cmd\n";
 				$multiplier = 1; 
 				$value = 0;
-				do{
+				do {
 					$digit = ord($this->read(1));
 					$value += ($digit & 127) * $multiplier; 
 					$multiplier *= 128;
-					}while (($digit & 128) != 0);
-				if($this->debug) echo "Fetching: $value\n";
+                } while (($digit & 128) != 0);
+
+				if ($this->debug) echo "Fetching: $value\n";
 				
-				if($value)
+				if ($value)
 					$string = $this->read($value);
 				
-				if($cmd){
-					switch($cmd){
+				if ($cmd) {
+					switch($cmd) {
 						case 3:
 							$this->message($string);
 						break;
@@ -296,16 +297,16 @@ class PheiMqtt
 					$this->timesinceping = time();
 				}
 			}
-			if($this->timesinceping < (time() - $this->keepalive )){
-				if($this->debug) echo "not found something so ping\n";
+			if ($this->timesinceping < (time() - $this->keepalive )) {
+				if ($this->debug) echo "not found something so ping\n";
 				$this->ping();	
 			}
 			
-			if($this->timesinceping<(time()-($this->keepalive*2))){
-				if($this->debug) echo "not seen a package in a while, disconnecting\n";
+			if ($this->timesinceping<(time()-($this->keepalive*2))) {
+				if ($this->debug) echo "not seen a package in a while, disconnecting\n";
 				fclose($this->socket);
 				$this->connect_auto(false);
-				if(count($this->topics))
+				if (count($this->topics))
 					$this->subscribe($this->topics);
 			}
 		}
@@ -315,55 +316,58 @@ class PheiMqtt
 	/* getmsglength: */
     public function getmsglength(&$msg, &$i)
     {
-		$multiplier = 1; 
-		$value = 0 ;
-		do {
-		  $digit = ord($msg{$i});
-		  $value += ($digit & 127) * $multiplier; 
-		  $multiplier *= 128;
-		  $i++;
-		} while (($digit & 128) != 0);
-		return $value;
+        $multiplier = 1; 
+        $value = 0 ;
+
+        do {
+            $digit = ord($msg{$i});
+            $value += ($digit & 127) * $multiplier; 
+            $multiplier *= 128;
+            $i++;
+        } while (($digit & 128) != 0);
+
+        return $value;
 	}
 
 	/* setmsglength: */
     public function setmsglength($len)
     {
-		$string = "";
-		do {
-		  $digit = $len % 128;
-		  $len = $len >> 7;
-		  // if there are more digits to encode, set the top bit of this digit
-		  if ( $len > 0 )
-		    $digit = ($digit | 0x80);
-		  $string .= chr($digit);
-		} while ( $len > 0 );
-		return $string;
+        $string = "";
+        do {
+            $digit = $len % 128;
+            $len = $len >> 7;
+            // if there are more digits to encode, set the top bit of this digit
+        if ( $len > 0 )
+            $digit = ($digit | 0x80);
+            $string .= chr($digit);
+        } while ( $len > 0 );
+
+        return $string;
 	}
 
 	/* strwritestring: writes a string to a buffer */
     public function strwritestring($str, &$i)
     {
-		$ret = " ";
-		$len = strlen($str);
-		$msb = $len >> 8;
-		$lsb = $len % 256;
-		$ret = chr($msb);
-		$ret .= chr($lsb);
-		$ret .= $str;
-		$i += ($len+2);
-		return $ret;
+        $ret = " ";
+        $len = strlen($str);
+        $msb = $len >> 8;
+        $lsb = $len % 256;
+        $ret = chr($msb);
+        $ret .= chr($lsb);
+        $ret .= $str;
+        $i += ($len+2);
+        return $ret;
 	}
 
     public function printstr($string)
     {
-		$strlen = strlen($string);
+        $strlen = strlen($string);
 
-        for($j=0;$j<$strlen;$j++){
+        for ($j=0;$j<$strlen;$j++) {
             $num = ord($string{$j});
-            if($num > 31) 
+            if ($num > 31)
                 $chr = $string{$j}; else $chr = " ";
-            printf("%4d: %08b : 0x%02x : %s \n",$j,$num,$num,$chr);
+                printf("%4d: %08b : 0x%02x : %s \n",$j,$num,$num,$chr);
         }
-	}
+    }
 }
